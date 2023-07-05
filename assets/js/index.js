52241198301f6598c6a72bcf1ddd90a194fae501
binukaK <binuka.kamesh@directfn.com>
2023-07-05 11:00:06 +0530

    edit pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
 // document.cookie = "firstName=BinukaKamesh; expires=Sun, 1 January 2030 12:00:00 UTC; path=/";
 // console.log(navigator.cookieEnabled);
-console.log("COOKIE IS: ", document.cookie);
-alert("COOKIES ARE:::" + document.cookie);
+// console.log("COOKIE IS: ", document.cookie);
+// alert("COOKIES ARE:::" + document.cookie);
